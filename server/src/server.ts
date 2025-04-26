d2fa051bddbba01ae3ec2b6dbb1c8b937d91f62d
Haritharun <1653224haritharun@gmail.com>
2025-04-26 14:02:36 +0530

    cors Updated

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,7 +13,7 @@ const app = express()
 
 app.use(express.json())
 
-app.use(cors())
+app.use(cors({origin:"https://collab-sphere-psi.vercel.app/"}))
 
 app.use(express.static(path.join(__dirname, "public"))) // Serve static files
 
